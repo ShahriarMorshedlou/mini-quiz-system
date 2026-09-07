19dc7ec012698539a157abf215334de62c91ab1b
Shahriar <ShahriarMorshedlou@gmail.com>
2026-09-07 21:18:07 +0330

    chore: initialize project and configure database

---
+++ src/main/java/com/shah/mini_quiz_system/MiniQuizSystemApplication.java
@@ -0,0 +1,13 @@
+package com.shah.mini_quiz_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniQuizSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniQuizSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shah/mini_quiz_system/MiniQuizSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shah.mini_quiz_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniQuizSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
